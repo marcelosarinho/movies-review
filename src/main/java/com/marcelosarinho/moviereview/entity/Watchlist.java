830c5191e393679701c19b84107f1f006569a8ba
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-09 19:27:20 -0300

    feat: Watchlist entity

---
+++ src/main/java/com/marcelosarinho/moviereview/entity/Watchlist.java
@@ -0,0 +1,13 @@
+package com.marcelosarinho.moviereview.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+public class Watchlist {
+}
