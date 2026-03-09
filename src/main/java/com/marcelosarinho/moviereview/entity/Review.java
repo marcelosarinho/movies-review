d8149219dd7b80f964085714cc27ed37ae3d68f9
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-09 19:15:34 -0300

    feat: Review entity

---
+++ src/main/java/com/marcelosarinho/moviereview/entity/Review.java
@@ -0,0 +1,43 @@
+package com.marcelosarinho.moviereview.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reviews")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Min(value = 1, message = "A avaliação mínima é 1!")
+    @Max(value = 5, message = "A avaliação máxima é 5!")
+    private Integer rating;
+
+    private String comment;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
