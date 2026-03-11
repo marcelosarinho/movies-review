8b969fae6176d94776c612ccd051c04c897c1720
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-11 17:11:50 -0300

    feat: insert method in UserController

--- src/main/java/com/marcelosarinho/moviereview/controller/UserController.java
+++ src/main/java/com/marcelosarinho/moviereview/controller/UserController.java
@@ -2,12 +2,12 @@ package com.marcelosarinho.moviereview.controller;
 
 import com.marcelosarinho.moviereview.entity.User;
 import com.marcelosarinho.moviereview.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,5 +28,11 @@ public class UserController {
     public ResponseEntity<Optional<User>> findById(@PathVariable Long id) {
         Optional<User> user = service.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@Valid @RequestBody User obj) {
+        obj = service.insert(obj);
+        return ResponseEntity.status(HttpStatus.CREATED).body(obj);
     }
 }
