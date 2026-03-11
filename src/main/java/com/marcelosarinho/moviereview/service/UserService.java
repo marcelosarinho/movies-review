787b2d987d66dbe39bf2ae9f607c57b40133c7a7
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-10 22:36:16 -0300

    feat: insert method in UserService

--- src/main/java/com/marcelosarinho/moviereview/service/UserService.java
+++ src/main/java/com/marcelosarinho/moviereview/service/UserService.java
@@ -22,4 +22,8 @@ public class UserService {
     public Optional<User> findById(Long id) {
         return repository.findById(id);
     }
+
+    public User insert(User obj) {
+        return repository.save(obj);
+    }
 }
