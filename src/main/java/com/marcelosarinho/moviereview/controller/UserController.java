a12487f979563123789b8c82e424ed64867caf83
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-10 22:49:14 -0300

    feat: findById in UserController

--- src/main/java/com/marcelosarinho/moviereview/controller/UserController.java
+++ src/main/java/com/marcelosarinho/moviereview/controller/UserController.java
@@ -5,10 +5,12 @@ import com.marcelosarinho.moviereview.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(name = "/users")
@@ -20,5 +22,11 @@ public class UserController {
     public ResponseEntity<List<User>> findAll() {
         List<User> users =  service.findAll();
         return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<User>> findById(@PathVariable Long id) {
+        Optional<User> user = service.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
