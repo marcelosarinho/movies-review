372d3a5ed0015d7037bc257b11ff77d6562409e8
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-10 22:35:15 -0300

    findByid method in UserService

--- src/main/java/com/marcelosarinho/moviereview/service/UserService.java
+++ src/main/java/com/marcelosarinho/moviereview/service/UserService.java
@@ -8,14 +8,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository repository;
 
-    @GetMapping
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public Optional<User> findById(Long id) {
+        return repository.findById(id);
     }
 }
