d1a657706825b54371e8a2b003fc702f5215dc93
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-10 22:09:06 -0300

    feat: Watchlist entity

--- src/main/java/com/marcelosarinho/moviereview/entity/Watchlist.java
+++ src/main/java/com/marcelosarinho/moviereview/entity/Watchlist.java
@@ -1,13 +1,47 @@
 package com.marcelosarinho.moviereview.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
-@Table
+@Table(
+        name = "watchlists",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"user_id", "movie_id"})
+        }
+)
 @NoArgsConstructor
 @AllArgsConstructor
 public class Watchlist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
