202ec654c6f6de7e06ad3cae87737193bde5d9ad
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-16 11:25:47 -0300

    feat: GenreDTO

---
+++ src/main/java/com/marcelosarinho/moviereview/dto/GenreDTO.java
@@ -0,0 +1,17 @@
+package com.marcelosarinho.moviereview.dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GenreDTO {
+    private Long id;
+
+    @NotBlank(message = "Nome do gênero é obrigatório!")
+    private String name;
+}
