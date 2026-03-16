368b8cee4559aee9aa212461a195c71043c10785
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-16 13:25:39 -0300

    feat: UserUpdateDTO

---
+++ src/main/java/com/marcelosarinho/moviereview/dto/user/UserUpdateDTO.java
@@ -0,0 +1,15 @@
+package com.marcelosarinho.moviereview.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class UserUpdateDTO {
+    @NotBlank(message = "Nome é obrigatório!")
+    private String name;
+
+    @Email(message = "Email deve ter formato válido!")
+    @NotBlank(message = "Email é obrigatório!")
+    private String email;
+}
