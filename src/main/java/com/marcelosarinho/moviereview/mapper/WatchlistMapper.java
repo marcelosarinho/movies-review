473b0e347b4edcc460cf3b5733a1f799f70e1cf8
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-24 13:20:36 -0300

    feat: WatchlistMapper interface

---
+++ src/main/java/com/marcelosarinho/moviereview/mapper/WatchlistMapper.java
@@ -0,0 +1,14 @@
+package com.marcelosarinho.moviereview.mapper;
+
+import com.marcelosarinho.moviereview.dto.watchlist.WatchlistDTO;
+import com.marcelosarinho.moviereview.entity.Watchlist;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface WatchlistMapper {
+    WatchlistDTO toWatchlistDTO(Watchlist watchlist);
+    Watchlist toWatchlistEntity(WatchlistDTO watchlistDTO);
+    List<WatchlistDTO> toWatchlistDTOList(List<Watchlist> watchlists);
+}
