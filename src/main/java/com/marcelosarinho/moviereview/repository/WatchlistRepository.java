522d780d90e2110d8400f38eb9266398297ec763
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-24 19:55:23 -0300

    feat: order watchlists by created at date

--- src/main/java/com/marcelosarinho/moviereview/repository/WatchlistRepository.java
+++ src/main/java/com/marcelosarinho/moviereview/repository/WatchlistRepository.java
@@ -2,9 +2,15 @@ package com.marcelosarinho.moviereview.repository;
 
 import com.marcelosarinho.moviereview.entity.Watchlist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface WatchlistRepository extends JpaRepository<Watchlist, Long> {
+    @Query("""
+        SELECT w FROM Watchlist w
+        WHERE w.user.id = :id
+        ORDER BY w.createdAt DESC
+    """)
     List<Watchlist> findByUserId(Long id);
 }
