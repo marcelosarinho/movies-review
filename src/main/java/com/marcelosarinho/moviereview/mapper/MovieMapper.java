2717b2e9c6b453b08e90c65cb00ccf3c84d38500
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-24 14:06:07 -0300

    feat: mapGenres method

--- src/main/java/com/marcelosarinho/moviereview/mapper/MovieMapper.java
+++ src/main/java/com/marcelosarinho/moviereview/mapper/MovieMapper.java
@@ -2,10 +2,12 @@ package com.marcelosarinho.moviereview.mapper;
 
 import com.marcelosarinho.moviereview.dto.movie.MovieCreateDTO;
 import com.marcelosarinho.moviereview.dto.movie.MovieDTO;
+import com.marcelosarinho.moviereview.entity.Genre;
 import com.marcelosarinho.moviereview.entity.Movie;
 import org.mapstruct.Mapper;
 
 import java.util.List;
+import java.util.Set;
 
 @Mapper(componentModel = "spring")
 public interface MovieMapper {
@@ -13,4 +15,8 @@ public interface MovieMapper {
     MovieDTO toMovieDTO(Movie movie);
     Movie toMovieEntity(MovieCreateDTO movieCreateDTO);
     List<MovieDTO> toMovieDTOList(List<Movie> movies);
+
+    default List<String> mapGenres(Set<Genre> genres) {
+        return genres.stream().map(Genre::getName).toList();
+    }
 }
